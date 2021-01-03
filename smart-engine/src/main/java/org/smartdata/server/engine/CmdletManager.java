a74e0e6f9ce41fb0b4792bb7a94b3c388de2c999
Eric Yin <whitman1314@163.com>
2021-01-03 15:49:54 +0800

    Track reload cmdlets

--- smart-engine/src/main/java/org/smartdata/server/engine/CmdletManager.java
+++ smart-engine/src/main/java/org/smartdata/server/engine/CmdletManager.java
@@ -213,6 +213,13 @@ public class CmdletManager extends AbstractService {
       cmdletInfos = metaStore.getCmdlets(CmdletState.DISPATCHED);
       if (cmdletInfos != null && cmdletInfos.size() != 0) {
         for (CmdletInfo cmdletInfo : cmdletInfos) {
+          //track reload cmdlets
+          CmdletDescriptor cmdletDescriptor =
+                  CmdletDescriptor.fromCmdletString(cmdletInfo.getParameters());
+          cmdletDescriptor.setRuleId(cmdletInfo.getRid());
+          if (cmdletDescriptor != null && !tracker.contains(cmdletDescriptor)) {
+            tracker.track(cmdletInfo.getCid(), cmdletDescriptor);
+          }
           List<ActionInfo> actionInfos = getActions(cmdletInfo.getAids());
           for (ActionInfo actionInfo: actionInfos) {
             actionInfo.setCreateTime(cmdletInfo.getGenerateTime());
@@ -225,6 +232,12 @@ public class CmdletManager extends AbstractService {
       cmdletInfos = metaStore.getCmdlets(CmdletState.PENDING);
       if (cmdletInfos != null && cmdletInfos.size() != 0) {
         for (CmdletInfo cmdletInfo : cmdletInfos) {
+          CmdletDescriptor cmdletDescriptor =
+                  CmdletDescriptor.fromCmdletString(cmdletInfo.getParameters());
+          cmdletDescriptor.setRuleId(cmdletInfo.getRid());
+          if (cmdletDescriptor != null && !tracker.contains(cmdletDescriptor)) {
+            tracker.track(cmdletInfo.getCid(), cmdletDescriptor);
+          }
           LOG.debug(String.format("Reload pending cmdlet: {}", cmdletInfo));
           List<ActionInfo> actionInfos = getActions(cmdletInfo.getAids());
           syncCmdAction(cmdletInfo, actionInfos);
@@ -233,6 +246,9 @@ public class CmdletManager extends AbstractService {
     } catch (MetaStoreException e) {
       LOG.error("DB connection error occurs when ssm is reloading cmdlets!");
       return;
+    } catch (ParseException ie) {
+      LOG.error("Cmdlet parameters format is not correct when ssm is"
+              + "reloading cmdlets!", ie);
     }
   }
 
@@ -474,24 +490,29 @@ public class CmdletManager extends AbstractService {
         }
       }
 
+      if (cmdletInfos.size() > 0) {
+        LOG.debug("Number of cmds {} to submit", cmdletInfos.size());
+        try {
+          metaStore.insertActions(
+                  actionInfos.toArray(new ActionInfo[actionInfos.size()]));
+          metaStore.insertCmdlets(
+                  cmdletInfos.toArray(new CmdletInfo[cmdletInfos.size()]));
+        } catch (MetaStoreException e) {
+          LOG.error("CmdletIds -> [ {} ], submit to DB error", cmdletInfos, e);
+        }
+      }
+
       for (CmdletInfo cmdletInfo : cmdletFinished) {
         idToCmdlets.remove(cmdletInfo.getCid());
-        tracker.untrack(cmdletInfo.getCid());
+        try {
+          tracker.untrack(cmdletInfo.getCid());
+        } catch (Exception e) {
+          LOG.warn("Untracking cmdlet failed.", e);
+        }
+
         for (Long aid : cmdletInfo.getAids()) {
           idToActions.remove(aid);
         }
-      }
-    }
-
-    if (cmdletInfos.size() > 0) {
-      LOG.debug("Number of cmds {} to submit", cmdletInfos.size());
-      try {
-        metaStore.insertActions(
-            actionInfos.toArray(new ActionInfo[actionInfos.size()]));
-        metaStore.insertCmdlets(
-            cmdletInfos.toArray(new CmdletInfo[cmdletInfos.size()]));
-      } catch (MetaStoreException e) {
-        LOG.error("CmdletIds -> [ {} ], submit to DB error", cmdletInfos, e);
       }
     }
 
